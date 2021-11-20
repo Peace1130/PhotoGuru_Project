5c5e1b941a5b2772450608abe153606d8e44f761
Kagiso <kagisokekana99@gmail.com>
2021-11-20 17:03:53 +0200

    First Commit

---
+++ PhotoGuru_Project/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PhotoGuru_Project.Areas.Identity.Data;
+using PhotoGuru_Project.Data;
+
+[assembly: HostingStartup(typeof(PhotoGuru_Project.Areas.Identity.IdentityHostingStartup))]
+namespace PhotoGuru_Project.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DBContextConnection")));
+
+                services.AddDefaultIdentity<AppUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+
+                })
+                        .AddEntityFrameworkStores<DBContext>();
+            });
+        }
+    }
+}
